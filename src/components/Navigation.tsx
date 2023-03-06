1d6c0b30fa8a5bb3f54efc32de03640ad3860894
feelflowofficialgit config --global user.email <feelflow.official@gmail.com>
2023-03-06 21:26:45 +0900

    Bootstrap Commit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,18 +3,44 @@ import st from '../styles/navigation.module.scss'
 
 export default function Navigation() {
   return (
-    <div className={st.div}>
-      <ul>
-        <li>
-          <Link href="/">Main</Link>
-        </li>
-        <li>
-          <Link href="/login">Login</Link>
-        </li>
-        <li>
-          <Link href="/signup">SignUp</Link>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" href="/">
+        Main
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          {/* <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li> */}
+          <li className="nav-item">
+            <Link className="nav-link" href="/login">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="/signup">
+              SignUp
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="/logout">
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
